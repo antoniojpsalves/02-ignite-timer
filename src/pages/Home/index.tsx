edb05377d326a3727ccc523934cc231a3035abf4
Antonio Alves <antonio.jpsalves@gmail.com>
2024-10-01 22:46:37 -0300

    :sparkles: feat: contador e botão estilizados.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,31 @@
-
-
+import { Play } from "phosphor-react";
+import { CountDownContainer, FormContainer, HomeContainer, Separator, StartCountdownButton } from "./styles";
 
 export function Home() {
   return (
-    <div>
-      <h1>Home page</h1>
-    </div>
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em: </label>
+          <input type="text" name="task" id="task" />
+
+          <label htmlFor="minutesAmount">durante</label>
+          <input type="number" name="minutesAmount" id="minutesAmount" />
+
+          <span>minutos.</span>
+        </FormContainer>
+        <CountDownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountDownContainer>
+        <StartCountdownButton type="submit">
+          <Play size="24" />
+          Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
   )
 }
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,86 @@
+import styled from 'styled-components'
+
+
+export const HomeContainer = styled.main`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 3.5rem;
+  }
+`
+
+export const FormContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: .5rem;
+  color: ${props => props.theme['gray-100']};
+
+  font-size: 1.125rem;
+  font-weight: bold;
+  flex-wrap: wrap;
+`
+
+export const CountDownContainer = styled.div`
+  font-family: 'Roboto Mono', monospace;
+  font-size: 10rem;
+  font-weight: 8rem;
+  color: ${props => props.theme['gray-100']};
+
+  display: flex;
+  gap: 1rem;
+
+  span {
+    background-color: ${props => props.theme['gray-700']};
+    padding: 2rem 1rem;
+    border-radius: 8px;
+  }
+`
+
+export const Separator = styled.div`
+  padding: 2rem 0;
+  color: ${props => props.theme['green-500']};
+  width: 4rem;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  text-align: center;
+`
+
+export const StartCountdownButton = styled.button`
+  width: 100%;
+  border: 0;
+  padding: 1rem;
+  border-radius: 8px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  gap: .5rem;
+  font-weight: bold;
+
+  cursor: pointer;
+
+  background-color: ${props => props.theme['green-500']};
+  color: ${props => props.theme['gray-100']};
+  transition: .3s;
+
+  &:disabled {
+    opacity: .7;
+    cursor: not-allowed;
+  }
+
+  &:not(:disabled):hover {
+    background-color: ${props => props.theme['green-700']};
+  }
+`
