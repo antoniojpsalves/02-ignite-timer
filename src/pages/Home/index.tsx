b6d1d0f7d36f244579865e7827041dd2b7340be8
Antonio Alves <antonio.jpsalves@gmail.com>
2024-10-01 23:18:03 -0300

    :sparkles: feat: layout da homepage concluido

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import { Play } from "phosphor-react";
-import { CountDownContainer, FormContainer, HomeContainer, Separator, StartCountdownButton } from "./styles";
+import { CountDownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from "./styles";
 
 export function Home() {
   return (
@@ -7,10 +7,30 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em: </label>
-          <input type="text" name="task" id="task" />
+          <TaskInput
+            id="task"
+            type="text"
+            name="task"
+            placeholder="Dê um nome para o seu projeto"
+            list="task-suggestions"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
-          <input type="number" name="minutesAmount" id="minutesAmount" />
+          <MinutesAmountInput
+            id="minutesAmount"
+            placeholder="00"
+            type="number"
+            name="minutesAmount"
+            step={5}
+            min={0}
+            max={60}
+          />
 
           <span>minutos.</span>
         </FormContainer>
